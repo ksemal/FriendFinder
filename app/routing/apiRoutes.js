c9535430f467576871bf5a980070802683f2b8b1
km <42688915+ksemal@users.noreply.github.com>
2019-01-27 12:02:10 -0500

    added compatibility logic for user's score

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,10 +3,35 @@ var router = express.Router();
 var friends = require("../data/friends");
 
 router.get("/friends", function(req, res) {
-  console.log("asasas");
-  res.json(friends);
+  res.send(friends);
 });
 
-router.post("/friends", function(req, res) {});
+router.post("/friends", function(req, res) {
+  var difference;
+  var min;
+  var name;
+  var current = req.body.scores;
+  friends.forEach(element => {
+    difference = 0;
+    for (var i = 0; i < element.scores.length; i++) {
+      if (element.scores[i] - current[i] < 0) {
+        difference += current[i] - element.scores[i];
+      } else {
+        difference += element.scores[i] - current[i];
+      }
+    }
+    if (difference < min || min === undefined) {
+      min = difference;
+      name = element.name;
+      photo = element.photo;
+    }
+    console.log("Min: " + min);
+    console.log("Dif:" + difference);
+  });
+  console.log("Name: " + name);
+
+  friends.push(req.body);
+  res.send(name);
+});
 
 module.exports = router;
